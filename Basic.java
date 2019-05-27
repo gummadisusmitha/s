f8075d2620d2f704b4123bdaf1fc035fdb5c207f
gummadisusmitha <45377615+gummadisusmitha@users.noreply.github.com>
2019-05-27 21:31:14 +0530

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -1,14 +1,27 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
- 
+
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Basic
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Hello World");
+		char ch=sc.next().charAt(0);
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+			System.out.println("Vowel");
+		}
+		else if(ch=='b'||ch=='c'||ch=='d'||ch=='f'||ch=='g'||ch=='h'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z')
+		{
+			System.out.println("Consonant");
+	
+		}
+		else
+		{
+			System.out.println("invalid");
+		}
 	}
 }
